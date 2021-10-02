e21f79336ba78383633bfe77743a4f00c72f3951
Nicholas Schwab <git@nicholas-schwab.de>
2021-10-02 14:11:14 +0200

    Fix syntax error and provide datasets at runtime.

--- kernels/main.cpp
+++ kernels/main.cpp
@@ -24,22 +24,22 @@ using namespace std;
 //    return out;
 //}
 
-int main() {
-    // k = 2.
+int main(int argc, char **argv) {
+    const auto datasets = [&]() {
+        if (argc == 0) {
+            return vector<std::string>{"ENZYMES", "IMDB-BINARY", "IMDB-MULTI", "NCI1", "NCI109", "PTC_FM"};
+        }
+        return std::vector<std::string>(argv, argv + argc);
+    }();
+
     {
-        vector<pair<string, bool>> datasets = {make_pair("ENZYMES", true), make_pair("IMDB-BINARY", false),
-                                               make_pair("IMDB-MULTI", false), make_pair("NCI1", true),
-                                               make_pair("NCI109", true), make_pair("PTC_FM", true),
-                                               make_pair("PROTEINS", true), make_pair("REDDIT-BINARY", false)};
-
-        for (auto &d: datasets) {
+        for (auto &ds: datasets) {
+            bool use_labels;
             {
-                string ds = std::get<0>(d);
-                bool use_labels = std::get<1>(d);
-
                 string kernel = "LWL2";
                 GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
                 gdb.erase(gdb.begin() + 0);
+                use_labels = gdb[0].has_node_labels();
                 vector<int> classes = AuxiliaryMethods::read_classes(ds);
 
                 GenerateTwo::GenerateTwo wl(gdb);
@@ -64,8 +64,6 @@ int main() {
             }
 
             {
-                string ds = std::get<0>(d);
-                bool use_labels = std::get<1>(d);
 
                 string kernel = "LWLP2";
                 GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
@@ -96,19 +94,13 @@ int main() {
     }
 
     {
-        vector<pair<string, bool>> datasets = {make_pair("ENZYMES", true), make_pair("IMDB-BINARY", false),
-                                               make_pair("IMDB-MULTI", false), make_pair("NCI1", true),
-                                               make_pair("NCI109", true), make_pair("PTC_FM", true),
-                                               make_pair("PROTEINS", true)};
-
-        for (auto &d: datasets) {
+        for (auto &ds: datasets) {
+            bool use_labels;
             {
-                string ds = std::get<0>(d);
-                bool use_labels = std::get<1>(d);
-
                 string kernel = "WL2";
                 GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
                 gdb.erase(gdb.begin() + 0);
+                use_labels = gdb[0].has_node_labels();
                 vector<int> classes = AuxiliaryMethods::read_classes(ds);
 
                 GenerateTwo::GenerateTwo wl(gdb);
@@ -133,9 +125,6 @@ int main() {
             }
 
             {
-                string ds = std::get<0>(d);
-                bool use_labels = std::get<1>(d);
-
                 string kernel = "DWL2";
                 GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
                 gdb.erase(gdb.begin() + 0);
@@ -166,18 +155,13 @@ int main() {
 
     // k = 3.
     {
-        vector<pair<string, bool>> datasets = {make_pair("ENZYMES", true), make_pair("IMDB-BINARY", false),
-                                               make_pair("IMDB-MULTI", false), make_pair("NCI1", true),
-                                               make_pair("NCI109", true), make_pair("PTC_FM", true)};
-
-        for (auto &d: datasets) {
+        for (auto &ds: datasets) {
+            bool use_labels;
             {
-                string ds = std::get<0>(d);
-                bool use_labels = std::get<1>(d);
-
                 string kernel = "LWL3";
                 GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
                 gdb.erase(gdb.begin() + 0);
+                use_labels = gdb[0].has_node_labels();
                 vector<int> classes = AuxiliaryMethods::read_classes(ds);
 
                 GenerateThree::GenerateThree wl(gdb);
@@ -195,16 +179,12 @@ int main() {
                         gm = wl.compute_gram_matrix(i, use_labels, "local", true, true);
                     }
 
-                    AuxiliaryMethods::write_libsvm(gm, classes,
-                                                   "./svm/GM/EXP/" + ds + "__" + kernel + "_" + to_string(i) +
-                                                   ".gram");
+                    AuxiliaryMethods::write_libsvm(
+                        gm, classes, "./svm/GM/EXP/" + ds + "__" + kernel + "_" + to_string(i) + ".gram");
                 }
             }
 
             {
-                string ds = std::get<0>(d);
-                bool use_labels = std::get<1>(d);
-
                 string kernel = "LWLP3";
                 GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
                 gdb.erase(gdb.begin() + 0);
@@ -225,26 +205,21 @@ int main() {
                         gm = wl.compute_gram_matrix(i, use_labels, "localp", true, true);
                     }
 
-                    AuxiliaryMethods::write_libsvm(gm, classes,
-                                                   "./svm/GM/EXP/" + ds + "__" + kernel + "_" + to_string(i) +
-                                                   ".gram");
+                    AuxiliaryMethods::write_libsvm(
+                        gm, classes, "./svm/GM/EXP/" + ds + "__" + kernel + "_" + to_string(i) + ".gram");
                 }
             }
         }
     }
 
     {
-        vector<pair<string, bool>> datasets = {make_pair("ENZYMES", true), make_pair("IMDB-BINARY", false),
-                                               make_pair("IMDB-MULTI", false), make_pair("PTC_FM", true)};
-
-        for (auto &d: datasets) {
+        for (auto &ds: datasets) {
+            bool use_labels;
             {
-                string ds = std::get<0>(d);
-                bool use_labels = std::get<1>(d);
-
                 string kernel = "WL3";
                 GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
                 gdb.erase(gdb.begin() + 0);
+                use_labels = gdb[0].has_node_labels();
                 vector<int> classes = AuxiliaryMethods::read_classes(ds);
 
                 GenerateThree::GenerateThree wl(gdb);
@@ -269,9 +244,6 @@ int main() {
             }
 
             {
-                string ds = std::get<0>(d);
-                bool use_labels = std::get<1>(d);
-
                 string kernel = "DWL3";
                 GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
                 gdb.erase(gdb.begin() + 0);
@@ -302,19 +274,13 @@ int main() {
 
     // Simple kernel baselines.
     {
-        vector<pair<string, bool>> datasets = {make_pair("ENZYMES", true), make_pair("IMDB-BINARY", false),
-                                               make_pair("IMDB-MULTI", false), make_pair("NCI1", true),
-                                               make_pair("NCI109", true), make_pair("PTC_FM", true),
-                                               make_pair("PROTEINS", true), make_pair("REDDIT-BINARY", false)};
-
-        for (auto &d: datasets) {
+        for (auto &ds: datasets) {
+            bool use_labels;
             {
-                string ds = std::get<0>(d);
-                bool use_labels = std::get<1>(d);
-
                 string kernel = "WL1";
                 GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
                 gdb.erase(gdb.begin() + 0);
+                use_labels = gdb[0].has_node_labels();
                 vector<int> classes = AuxiliaryMethods::read_classes(ds);
 
                 ColorRefinement::ColorRefinementKernel wl(gdb);
@@ -339,9 +305,6 @@ int main() {
             }
 
             {
-                string ds = std::get<0>(d);
-                bool use_labels = std::get<1>(d);
-
                 string kernel = "WLOA";
                 GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
                 gdb.erase(gdb.begin() + 0);
@@ -369,9 +332,6 @@ int main() {
             }
 
             {
-                string ds = std::get<0>(d);
-                bool use_labels = std::get<1>(d);
-
                 string kernel = "SP";
                 GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
                 gdb.erase(gdb.begin() + 0);
@@ -393,9 +353,6 @@ int main() {
             }
 
             {
-                string ds = std::get<0>(d);
-                bool use_labels = std::get<1>(d);
-
                 string kernel = "GR";
                 GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
                 gdb.erase(gdb.begin() + 0);
@@ -414,92 +371,6 @@ int main() {
 
                 AuxiliaryMethods::write_libsvm(gm, classes,
                                                "./svm/GM/EXP/" + ds + "__" + kernel + "_" + to_string(0) + ".gram");
-            }
-        }
-    }
-
-
-    // Larger Datasets.
-    vector<string> d = {"Yeast", "YeastH", "UACC257", "UACC257H", "OVCAR-8", "OVCAR-8H"};
-    for (auto &ds: d) {
-        bool use_labels = true;
-        bool use_edge_labels = true;
-        {
-            string kernel = "LWL2";
-            GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
-            gdb.erase(gdb.begin() + 0);
-            vector<int> classes = AuxiliaryMethods::read_classes(ds);
-
-            GenerateTwo::GenerateTwo wl(gdb);
-            for (uint i = 0; i <= 5; ++i) {
-                cout << ds + "__" + kernel + "_" + to_string(i) << endl;
-                GramMatrix gm;
-
-                if (i == 5) {
-                    high_resolution_clock::time_point t1 = high_resolution_clock::now();
-                    gm = wl.compute_gram_matrix(i, use_labels, use_edge_labels, "local", true, false);
-                    high_resolution_clock::time_point t2 = high_resolution_clock::now();
-                    auto duration = duration_cast<seconds>(t2 - t1).count();
-                    cout << duration << endl;
-                } else {
-                    gm = wl.compute_gram_matrix(i, use_labels, use_edge_labels, "local", true, false);
-                }
-
-                AuxiliaryMethods::write_sparse_gram_matrix(gm, "./svm/GM/EXPSPARSE/" + ds +
-                                                              "__" + kernel + "_" + to_string(i));
-            }
-        }
-
-        {
-            string kernel = "LWLP2";
-            GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
-            gdb.erase(gdb.begin() + 0);
-            vector<int> classes = AuxiliaryMethods::read_classes(ds);
-
-            GenerateTwo::GenerateTwo wl(gdb);
-            for (uint i = 0; i <= 5; ++i) {
-                cout << i << endl;
-                cout << ds + "__" + kernel + "_" + to_string(i) << endl;
-                GramMatrix gm;
-
-                if (i == 5) {
-                    high_resolution_clock::time_point t1 = high_resolution_clock::now();
-                    gm = wl.compute_gram_matrix(i, use_labels, use_edge_labels, "localp", true, false);
-                    high_resolution_clock::time_point t2 = high_resolution_clock::now();
-                    auto duration = duration_cast<seconds>(t2 - t1).count();
-                    cout << duration << endl;
-                } else {
-                    gm = wl.compute_gram_matrix(i, use_labels, use_edge_labels, "localp", true, false);
-                }
-
-                AuxiliaryMethods::write_sparse_gram_matrix(gm, "./svm/GM/EXPSPARSE/" + ds +
-//                                                               "__" + kernel + "_" + to_string(i));
-            }
-        }
-
-        {
-            string kernel = "WL";
-            GraphDatabase gdb = AuxiliaryMethods::read_graph_txt_file(ds);
-            gdb.erase(gdb.begin() + 0);
-            vector<int> classes = AuxiliaryMethods::read_classes(ds);
-
-            ColorRefinement::ColorRefinementKernel wl(gdb);
-            for (uint i = 0; i <= 5; ++i) {
-                cout << ds + "__" + kernel + "_" + to_string(i) << endl;
-                GramMatrix gm;
-
-                if (i == 5) {
-                    high_resolution_clock::time_point t1 = high_resolution_clock::now();
-                    gm = wl.compute_gram_matrix(i, use_labels, use_edge_labels, false, false);
-                    high_resolution_clock::time_point t2 = high_resolution_clock::now();
-                    auto duration = duration_cast<seconds>(t2 - t1).count();
-                    cout << duration << endl;
-                } else {
-                    gm = wl.compute_gram_matrix(i, use_labels, use_edge_labels, false, false);
-                }
-
-                AuxiliaryMethods::write_sparse_gram_matrix(gm, "./svm/GM/EXPSPARSE/" + ds +
-                                                               "__" + kernel + "_" + to_string(i));
             }
         }
     }
